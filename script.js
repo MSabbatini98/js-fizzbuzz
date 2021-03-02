4024ff8ea10d779efe7ff68369f487c8c4f817c6
MSebb23 <matteosabbatini23@gmail.com>
2021-03-02 14:25:28 +0100

    Added Extras
    100  is now a variable given by input

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var counter = 0;
-for (var i = 0; i != 100; i++) {
+//********************** */ EXTRASSS
+var max_num = parseInt(prompt("Inserisci il numero massimo di cui vuoi calcolare FizzBuzz, maggiore di 0"));
+// validatore di max_num
+for (var j = 10; j > 0; j--) {
+    if (!(!isNaN(max_num) && max_num > 0)){
+        var max_num = parseInt(prompt("Inserisci il numero massimo di cui vuoi calcolare FizzBuzz, maggiore di 0")); 
+    }
+}
+
+
+for (var i = 0; i != max_num; i++) {
     counter += 1;
     console.log(counter);
     if ((counter % 3) == 0 && (counter % 5)!= 0 ){
