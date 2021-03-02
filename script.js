9e018517c774ccdacef44cb8e1c2fe76481f2a9a
MSebb23 <matteosabbatini23@gmail.com>
2021-03-02 15:55:33 +0100

    Changed if statements
    Added some comments too

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// variabile globale
 var counter = 0;
+
 //********************** */ EXTRASSS
 var max_num = parseInt(prompt("Inserisci il numero massimo di cui vuoi calcolare FizzBuzz, maggiore di 0"));
+
 // validatore di max_num
 for (var j = 10; j > 0; j--) {
     if (!(!isNaN(max_num) && max_num > 0)){
@@ -8,7 +11,27 @@ for (var j = 10; j > 0; j--) {
     }
 }
 
-
+// Itero sulla variabile max_num
+for (var i = 0; i != max_num; i++) {
+    counter += 1;
+    console.log(counter);
+    // se counter è divisibile per 3...
+    if ((counter % 3) == 0 ){
+        // ... e per 5
+        if ((counter % 5) == 0){
+            document.getElementById("outcome").innerHTML += "<span id ='fizzbuzz'> FizzBuzz </span> <br>";
+        } else {
+            document.getElementById("outcome").innerHTML += "<span id ='fizz'> Fizz </span> <br>";
+        }
+    // se counter è divisibile per 5
+    } else if ((counter % 5)== 0 ) {
+        document.getElementById("outcome").innerHTML += "<span id ='buzz'> Buzz </span> <br>";
+    // se counter non è multiplo di 3 o 5
+    } else {
+        document.getElementById("outcome").innerHTML +=  counter + "<br>";
+    }
+}
+/*
 for (var i = 0; i != max_num; i++) {
     counter += 1;
     console.log(counter);
@@ -22,3 +45,4 @@ for (var i = 0; i != max_num; i++) {
         document.getElementById("outcome").innerHTML +=  counter + "<br>";
     }
 }
+*/
